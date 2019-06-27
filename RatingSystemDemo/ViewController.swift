be34bad825731b8acfcfe7dc58f913596096d5ba
Vachanashreebiradar <vachanashreeb9805@gmail.com>
2019-06-27 11:00:20 +0530

    initial commit

---
+++ RatingSystemDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RatingSystemDemo
+//
+//  Created by Vachanashree Biradar on 27/06/19.
+//  Copyright © 2019 Vachanashree. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
